58b39d7f1205716b73e58165af595dd31864b0ad
dpa722 <38964449+dpa722@users.noreply.github.com>
2018-10-25 00:03:16 -0400

    Add files via upload

---
+++ direcciones/footer.php
@@ -0,0 +1,50 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-xs-6">
+				<div class="footer">
+					<h3 class="footer-title">Informacion</h3>
+					<ul class="footer-links">
+						<li><a href="#">¿Quienes Somos?</a></li>
+						<li><a href="#">Contacto y ayuda</a></li>
+						<li><a href="#">Seguridad</a></li>
+						<li><a href="#">Terminos & Condiciones</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="clearfix visible-xs"></div>
+			<div class="col-md-3 col-xs-6">
+				<div class="footer">
+					<h3 class="footer-title">Servicios</h3>
+					<ul class="footer-links">
+						<li><a href="#">Prinera Publicacion</a></li>
+						<li><a href="#">Organizacion</a></li>
+						<li><a href="#">Ordenes</a></li>
+						<li><a href="#">Anuncios Importantes</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3 col-xs-6">
+				<div class="footer">
+					<h3 class="footer-title">Acerca de nosotros</h3>
+					<ul class="footer-links">
+						<li href="#">Contactate con Nosotros</li>
+						<li><a href="#">Correo Electronico</a></li>
+						<li><a href="#"># Celular</a></li>
+						<li><a href="#">Direccion</a></li>
+
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<span class="copyright">
+					Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Estudiantes de Ing. de Sistemas de la Universidad Mayor de San Simon.
+				</span>
+			</div>
+		</div>
+	</div>
+</footer>
