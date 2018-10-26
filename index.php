974ed3a26d30dcd4fb201965df3010f618a037d3
dpa722 <daniel.dpa722@gmail.com>
2018-10-25 23:24:12 -0400

    daniel arreglo lo que fallaba

--- index.php
+++ index.php
@@ -18,32 +18,72 @@
 <body>	
 
 
-<header>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-		<a class="navbar-brand" href="index.php">
-			<img src="img/lupa.png">
-			<a class="nav-link" href="#">TBD <span class="sr-only">(current)</span></a>
-		</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+	<header>
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand" href="#">TBD</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 
-		<div class="collapse navbar-collapse" id="navbarColor01">
-			
-			<form class="form-inline my-2 my-lg-0">
-				<input class="form-control mr-sm-2" type="text" placeholder="¿Que estas buscando?">
-				<button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
-			</form>
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item"><a class="nav-link" href="ingresar.php">Ingresar</a></li>
-				<li class="nav-item"><a class="nav-link" href=" registrarse.php">Registrarse</a></li>
-				<li class="nav-item"><a class="nav-link" href="#">Ayuda</a></li>
-			</ul>
+				<div class="collapse navbar-collapse" id="navbarColor03">
+					<form class="form-inline my-2 my-lg-0 buscar">
+						<input class="form-control mr-sm-2" type="text" placeholder="¿Que estas buscando?">
+						<button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
+					</form>
+					<ul class="navbar-nav mr-auto derecha">
+						<li class="nav-item"><a class="nav-link" href="ingresar.php">Ingresar</a></li>
+						<li class="nav-item"><a class="nav-link" href="registrarse.php">Registrarse</a></li>
+						<li class="nav-item"><a class="nav-link" href="ayuda.php">Ayuda</a></li>
+					</ul>
+				</div>
+			</nav>
 		</div>
-	</nav>
-</header>
+	</header>
 
-	
+
+
+
+
+
+
+
+
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 
 
@@ -254,7 +294,7 @@
 				</div>
 			</div>
 		</div>
-	</footer
+	</footer>
 
 	
 	<script type="text/javascript" src="js/jquery.min.js"></script>
